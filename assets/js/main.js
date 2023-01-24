80418295f6448d13cdac87bfe4716ab5cabf640e
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-24 14:23:48 +0100

    frage

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+console.log("test");
+
+/* 
+Frage
+
+toLowerCase   = alles klein geschrieben
+toUpperCase     = alles groß geschrieben
+
+const text = "Sam is going to codingschool";
+
+in Variablen
+
+        replace in Variable mit school anstatt codingschool
+        dann wörter Suchen     
+            Positionen suchen länge suchen 
+            dann verändern in groß oder klein geschrieben
+
+in html
+    1. SAM IS GOING TO SCHOOL
+    2. sam is going to school
+    3. SAM is going to SCHOOL
+    4. sam IS GOING TO school
+    5. Sam Is Going To School
+
+*/
+
+const text = "Sam is going to codingschool";
+console.log(text);
